3089d59986d01a2b14ccd358cab90a2c36d5e75d
kuniakisnow <kuniaki.ito@gmail.com>
2026-06-01 22:09:11 +0900

    fix: |base(reading)→<ruby>タグ直接置換、table誤生成を根本修正

--- _plugins/ruby.rb
+++
@@ -1,8 +0,0 @@
-# _plugins/ruby.rb
-# Converts |base(reading) or |base（reading） to <ruby>base<rt>reading</rt></ruby>
-# before Markdown rendering. This also prevents the pipe character from
-# being interpreted as a table delimiter.
-
-Jekyll::Hooks.register [:posts, :pages], :pre_render do |doc|
-  doc.content.gsub!(/\|([^\|(（]+)[(（]([^)）]+)[)）]/) { "<ruby>#{$1}<rt>#{$2}</rt></ruby>" }
-end
